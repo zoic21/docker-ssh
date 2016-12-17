be66d7a23acc7c566acdea096b4f2b801c11a100
Loïc <loic@jeedom.com>
2016-12-17 15:19:43 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,5 +8,17 @@ else
 	echo "root:${ROOT_PASSWORD}" | chpasswd
 fi
 
+if [ -f /root/helper ]; then
+    cd /root/helper
+    git reset --hard HEAD
+    git pull
+else
+   cd /root
+   git clone --depth 1 https://github.com/jeedom/helper.git
+fi
+
+dos2unix /root/helper/ssh/connection.sh
+dos2unix /root/helper/ssh/multissh.sh
+
 /usr/bin/supervisord
 
